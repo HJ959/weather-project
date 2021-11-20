a1075ca0b63f10ffe8ac7b7575e0a5ff912097ba
HJ959 <henryjaames@gmail.com>
2021-11-20 11:02:46 +0000

    updated

--- JS/main-tone.js
+++ JS/main-tone.js
@@ -72,7 +72,7 @@ function httpGet(theUrl) {
 /////////////////////////////////////////////////////////////////////////
 function makeid(length) {
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() *
