34845c0c763d45c76be6431cfa247a88d6ec6319
HJ959 <henryjaames@gmail.com>
2021-11-21 16:15:42 +0000

    Added netlify functions file

---
+++ netlify/functions/scripts.js
@@ -0,0 +1,11 @@
+import fetch from "node-fetch"
+
+exports.handler = async (event, context, callback) => {
+    const youtubeAPI = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=${process.env.BEHANCE_API_KEY}`
+    const response = await fetch(youtubeAPI)
+    const data = await response.json()
+    return  {
+      statusCode: 200,
+      body: JSON.stringify(data.projects)
+    }
+}
