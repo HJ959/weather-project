1f6bb5d2ace35a49f25b223ef558e0f4a4e57d62
HJ959 <henryjaames@gmail.com>
2021-11-22 09:02:00 +0000

    Updated netlify function name

--- netlify/functions/searchYoutube.js
+++ netlify/functions/searchYoutube.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch"
 
 exports.handler = async (event, context, callback) => {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     const youtubeAPI = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=${process.env.BEHANCE_API_KEY}`
     const response = await fetch(youtubeAPI)
     const data = await response.json()
