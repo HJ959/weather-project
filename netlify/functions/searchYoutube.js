e13d5c87f057c5592521ca779630050eed30a706
HJ959 <henryjaames@gmail.com>
2021-11-22 09:34:56 +0000

    Updated netlify function name

--- netlify/functions/searchYoutube.js
+++ netlify/functions/searchYoutube.js
@@ -2,8 +2,8 @@
 
 exports.handler = async (event, context, callback) => {
   /* parse the string body into a useable JS object */
-  const data = JSON.parse(event.body)
-  console.log("Function `todo-create` invoked", data)
+  const jsonBody = JSON.parse(event.body)
+  console.log("Function `todo-create` invoked", jsonBody)
   const youtubeAPI = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=${process.env.BEHANCE_API_KEY}`
   const response = await fetch(youtubeAPI)
   const data = await response.json()
