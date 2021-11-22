509fd88ad231318d16450885586d2a2ca9538d61
HJ959 <henryjaames@gmail.com>
2021-11-22 20:47:07 +0000

    Accept JSON

--- netlify/functions/searchYoutube.js
+++ netlify/functions/searchYoutube.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
 
-YOUTUBE_API_ENDPOINT = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key='
+YOUTUBE_API_ENDPOINT = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=' + String(process.env.YT_API_KEY)
 
 exports.handler = async (event, context) => {
   let response
   try {
-    response = await fetch(YOUTUBE_API_ENDPOINT + String(process.env.YT_API_KEY))
+    response = await fetch(YOUTUBE_API_ENDPOINT)
     // handle response
   } catch (err) {
     return {
