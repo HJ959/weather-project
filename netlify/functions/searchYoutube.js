4943cb2041397138bf457965ffa24291fd31609a
HJ959 <henryjaames@gmail.com>
2021-11-23 20:16:01 +0000

    Store the data from the API call to a variable

--- netlify/functions/searchYoutube.js
+++ netlify/functions/searchYoutube.js
@@ -1,25 +1,39 @@
 const fetch = require('node-fetch');
 
-YOUTUBE_API_ENDPOINT = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=' + String(process.env.YT_API_KEY)
+//YOUTUBE_API_ENDPOINT = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=' + String(process.env.YT_API_KEY)
 
-exports.handler = async (event, context) => {
-  let response
-  try {
-    response = await fetch(YOUTUBE_API_ENDPOINT)
-    // handle response
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message
-      })
-    }
-  }
+const https = require('https')
+let url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=' + String(process.env.YT_API_KEY)
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: YOUTUBE_API_ENDPOINT
+exports.handler = async function (event) {
+  const promise = new Promise(function (resolve, reject) {
+    https.get(url, (res) => {
+      resolve(res.statusCode)
+    }).on('error', (e) => {
+      reject(Error(e))
     })
-  }
+  })
+  return promise
 }
+
+// exports.handler = async (event, context) => {
+//   let response
+//   try {
+//     response = await fetch(YOUTUBE_API_ENDPOINT)
+//     // handle response
+//   } catch (err) {
+//     return {
+//       statusCode: err.statusCode || 500,
+//       body: JSON.stringify({
+//         error: err.message
+//       })
+//     }
+//   }
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       data: YOUTUBE_API_ENDPOINT
+//     })
+//   }
+// }
