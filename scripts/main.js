92a5fe161168e3192e76b9dca9252f8066ea9a8b
HJ959 <henryjaames@gmail.com>
2021-11-23 18:05:23 +0000

    Store the data from the API call to a variable

--- scripts/main.js
+++ scripts/main.js
@@ -128,7 +128,7 @@ fetch('/.netlify/functions/searchYoutube')
       // Examine the text in the response
       response.json().then(function(data) {
         console.log(data); 
-        youtubeSearchData = data;
+        youtubeSearchData = JSON.parse(data);
       });
     }
   )
