764f44cafa835797f89cfa2e92da0bae5dbe8052
HJ959 <henryjaames@gmail.com>
2021-11-23 17:53:14 +0000

    Store the data from the API call to a variable

--- netlify/functions/searchYoutube.js
+++ netlify/functions/searchYoutube.js
@@ -11,9 +11,9 @@ exports.handler = async (event, context) => {
     return {
       statusCode: err.statusCode || 500,
       body: JSON.stringify({
-        error: err.message
+        error: process.env.YT_API_KEY
       })
     }
   }
-  return response
+  return process.env.YT_API_KEY
 }
