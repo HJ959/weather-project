8336a17de2493a7a025081e4ceac28423648b5d6
HJ959 <henryjaames@gmail.com>
2021-11-24 17:16:47 +0000

    Updated the searchYoutube function to use OAUth2.0

--- netlify/functions/searchYoutube.js
+++ netlify/functions/searchYoutube.js
@@ -1,12 +1,26 @@
-const fetch = require('node-fetch');
+const {google} = require('googleapis');
+const path = require('path');
+const {authenticate} = require('@google-cloud/local-auth');
+var OAuth2 = google.auth.OAuth2;
 
 YOUTUBE_API_ENDPOINT = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=live%20public%20webcam&key=' + String(process.env.YT_API_KEY)
 
+// initialize the Youtube API library
+const youtube = google.youtube('v3');
+
 exports.handler = async (event, context) => {
-  let response
   try {
-    response = await fetch(YOUTUBE_API_ENDPOINT)
-    // handle response
+    const auth = await authenticate({
+      keyfilePath: process.env.OAUTH_JSON,
+      scopes: ['https://www.googleapis.com/auth/youtube'],
+    });
+    google.options({auth});
+
+    const res = await youtube.search.list({
+      part: 'id,snippet',
+      q: 'Node.js on Google Cloud',
+    });
+
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
@@ -19,7 +33,7 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data: response
+      data: res
     })
   }
 }
