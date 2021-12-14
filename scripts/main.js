ae5e321e7c39b0aa2cca6439dda4d8b6170b8bf1
HJ959 <henryjaames@gmail.com>
2021-12-14 09:54:48 +0000

    Created the weatherLookup AWS lambada function and added a call to it in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,110 @@
-let oscOne, oscTwo, ocsThree, oscFour;
-let filter, filterTwo, filterThree, filterFour, pingPong
+"use strict";
+// Tone JS vars
+let oscOne, oscTwo, oscThree, oscFour;
+let filter, filterTwo, filterThree, filterFour, pingPong;
+let meter, meterTwo, meterThree;
+let rms, rmsTwo, rmsThree;
+
+// location vars
+let lat, lon, lonLat;
+
+// video vars
+let youtubeIDs;
 
 /////////////////////////////////////////////////////////////////////////
-function setup() {
-  // add some super nice mega ping pong delay
-  pingPong = new Tone.PingPongDelay(30, 0.7).toDestination();
-
-  // filter to make less horrible
-  filter = new Tone.AutoFilter(0.13).toDestination().connect(pingPong);
-  filterTwo = new Tone.AutoFilter(0.05).toDestination().connect(pingPong);
-  filterThree = new Tone.AutoFilter(0.07).toDestination().connect(pingPong);
-  filterFour = new Tone.AutoFilter(0.11).toDestination().connect(pingPong);
-
-  // create ocsillator one
-  oscOne = new Tone.PulseOscillator({
-    "detune": 5,
-    "frequency": "F3",
-    "phase": 0.25,
-    "volume": -32
-  }).connect(filter);
-
-  // create ocsillator two
-  oscTwo = new Tone.PulseOscillator({
-    "detune": -5,
-    "frequency": "C3",
-    "phase": -0.25,
-    "volume": -32
-  }).connect(filterTwo);
-
-  // create ocsillator three
-  oscThree = new Tone.PulseOscillator({
-    "detune": 7,
-    "frequency": "E3",
-    "phase": 0.5,
-    "volume": -32
-  }).connect(filterThree);
-
-  // create ocsillator four
-  oscFour = new Tone.PulseOscillator({
-    "detune": -7,
-    "frequency": "G3",
-    "phase": 0.3,
-    "volume": -32
-  }).connect(filterFour);
+// on script load get the location and store to lat and lon
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
 }
+
+function showPosition(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+}
+getLocation();
+
+//////////////////////////////////////////////////////////////////////////////
+// Function using fetch to POST to our API endpoint
+function weatherLookup(data) {
+  return fetch('/.netlify/functions/weatherLookup', {
+    body: JSON.stringify(data),
+    method: 'POST'
+  }).then(response => {
+    return response.json()
+  })
+}
+
+// Todo data
+lonLat = {
+  latitude: lat,
+  longitude: lon,
+}
+
+// create it!
+weatherLookup(lonLat).then((response) => {
+  console.log('API response', response)
+  // set app state
+}).catch((error) => {
+  console.log('API error', error)
+})
+//////////////////////////////////////////////////////////////////////////////
+
+
+// add some super nice mega ping pong delay
+pingPong = new Tone.PingPongDelay(30, 0.7).toDestination();
+
+// filter to make less horrible
+filter = new Tone.AutoFilter(0.13).toDestination().connect(pingPong);
+filterTwo = new Tone.AutoFilter(0.05).toDestination().connect(pingPong);
+filterThree = new Tone.AutoFilter(0.07).toDestination().connect(pingPong);
+filterFour = new Tone.AutoFilter(0.11).toDestination().connect(pingPong);
+
+// set up some RMS meters for use with the visuals
+meter = new Tone.Meter();
+meterTwo = new Tone.Meter();
+meterThree = new Tone.Meter();
+
+// connect RMS meters to the filters
+filter.connect(meter);
+filterTwo.connect(meterTwo);
+filterThree.connect(meterThree);
+
+// create ocsillator one
+oscOne = new Tone.PulseOscillator({
+  "detune": 5,
+  "frequency": "F3",
+  "phase": 0.25,
+  "volume": -32
+}).connect(filter);
+
+// create ocsillator two
+oscTwo = new Tone.PulseOscillator({
+  "detune": -5,
+  "frequency": "C3",
+  "phase": -0.25,
+  "volume": -32
+}).connect(filterTwo);
+
+// create ocsillator three
+oscThree = new Tone.PulseOscillator({
+  "detune": 7,
+  "frequency": "E3",
+  "phase": 0.5,
+  "volume": -32
+}).connect(filterThree);
+
+// create ocsillator four
+oscFour = new Tone.PulseOscillator({
+  "detune": -7,
+  "frequency": "G3",
+  "phase": 0.3,
+  "volume": -32
+}).connect(filterFour);
+
 
 /////////////////////////////////////////////////////////////////////////
 function songStart(time) {
@@ -52,6 +113,16 @@ function songStart(time) {
   oscTwo.start();
   oscThree.start();
   oscFour.start();
+
+  // change the opacity of the videos 
+  // grab the video elements
+  let video1 = document.getElementById('playerOne');
+  let video2 = document.getElementById('playerTwo');
+  let video3 = document.getElementById('playerThree');
+  rms = meter.getValue();
+  rms = scale(rms, -100, 6, 0.1, 0.9)
+  video1.style.opacity = rms;
+  video1.style.filter = 'alpha(opacity=' + String(scale(rms, 0.1, 1.0, 1, 100)) + ')'; // IE fallback
 }
 /////////////////////////////////////////////////////////////////////////
 function songStop(time) {
@@ -62,14 +133,9 @@ function songStop(time) {
   oscFour.stop();
 }
 /////////////////////////////////////////////////////////////////////////
-function httpGet(theUrl) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", theUrl, false); // false for synchronous request
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
-}
+// List of random youtube embeded IDs
+youtubeIDs = ['viOkh9al0xM', 'AdUw5RdyZxI', 'Cp4RRAEgpeU', 'qQZILMyW88o', 'F109TZt3nRc', 'EoTrcNvDykY', '5jcU2MVkEy8', 'SQfE_-sSdc4', 'XBPjVzSoepo', 'aofxeUrkYQ4', 'A7oGJdUmVkg', 'QpqsJKI0Wfk', 'tbQqobLDCSg', 'jrVrO7VDccI', 'CRbOu0ygzLA', 'QbKEtWF6U2o']
 
-/////////////////////////////////////////////////////////////////////////
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
@@ -83,7 +149,7 @@ var player;
 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('playerOne', {
-    videoId: 'viOkh9al0xM',
+    videoId: youtubeIDs[getRandomInt(0, youtubeIDs.length)],
     playerVars: {
       'autoplay': 1,
       'mute': 1,
@@ -95,7 +161,19 @@ function onYouTubeIframeAPIReady() {
     }
   });
   playerTwo = new YT.Player('playerTwo', {
-    videoId: 'AdUw5RdyZxI',
+    videoId: youtubeIDs[getRandomInt(0, youtubeIDs.length)],
+    playerVars: {
+      'autoplay': 1,
+      'mute': 1,
+      'controls': 0,
+      'rel': 0
+    },
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+  playerThree = new YT.Player('playerThree', {
+    videoId: youtubeIDs[getRandomInt(0, youtubeIDs.length)],
     playerVars: {
       'autoplay': 1,
       'mute': 1,
@@ -114,7 +192,14 @@ function onPlayerReady(event) {
 }
 
 //////////////////////////////////////////////////////////////////////////
-let youtubeSearchData;
-
-youtubeSearchData = fetch('/.netlify/functions/searchYoutube');
-  
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+/////////////////////////////////////////////////////////////////////////
+function scale(number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+/////////////////////////////////////////////////////////////////////////
