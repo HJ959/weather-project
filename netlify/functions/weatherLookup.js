047d4454c974ef05df26604b2455275592f6daaa
HJ959 <henryjaames@gmail.com>
2021-12-14 10:01:19 +0000

    test with cat facts to test I can successfully bring back a json

--- netlify/functions/weatherLookup.js
+++ netlify/functions/weatherLookup.js
@@ -1,3 +1,28 @@
+// // import fetch from 'node-fetch';
+// const fetch = require('node-fetch')
+
+// const checkStatus = (res) => {
+//   if (res.ok) { // res.status >= 200 && res.status < 300
+//       return res.json()
+//   } else {
+//       throw new Error(res.statusText);
+//   }
+// }
+
+// exports.handler = async function(event, context, callback) {
+//   try {
+//     const coords = JSON.parse(event.body)
+//     const response = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.latitude + "&lon=" + coords.longitude + "&units=metric&appid=" + process.env.WEATHER_API_KEY)
+//     const data = await checkStatus(response)
+//     callback(null, {
+//       statusCode: 200,
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(data)
+//     })
+//   } catch (error) {
+//     callback(error)
+//   }
+// }
 // import fetch from 'node-fetch';
 const fetch = require('node-fetch')
 
@@ -11,8 +36,7 @@ const checkStatus = (res) => {
 
 exports.handler = async function(event, context, callback) {
   try {
-    const coords = JSON.parse(event.body)
-    const response = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.latitude + "&lon=" + coords.longitude + "&units=metric&appid=" + process.env.WEATHER_API_KEY)
+    const response = await fetch('https://cat-fact.herokuapp.com/facts')
     const data = await checkStatus(response)
     callback(null, {
       statusCode: 200,
