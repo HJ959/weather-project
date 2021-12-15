d63a0b71a8dfa840fc9d09a33631b9cd52ce6e6f
HJ959 <henryjaames@gmail.com>
2021-12-15 21:26:24 +0000

    Added rotate on some of the video divs and randomized the steps taken by the video fader

--- scripts/userInteraction.js
+++ scripts/userInteraction.js
@@ -24,14 +24,6 @@ function songStop(time) {
 document.querySelector('#wholePage')?.addEventListener('mousedown', function (event) {
   // if mousedown and the flag is set to Start
   if (startStopFlag === 'Start') {
-    // grab the weather data from the API on mousedown
-    weatherLookup(latLon).then((response) => {
-      console.log('API response', response)
-      // set app state
-    }).catch((error) => {
-      console.log('API error', error)
-    })
-
     filter.start();
     oscOne.start();
     oscTwo.start();
@@ -42,19 +34,16 @@ document.querySelector('#wholePage')?.addEventListener('mousedown', function (ev
     playerTwo.seekTo(getRandomInt(0,10800));
     playerThree.seekTo(getRandomInt(0,10800));
     playerFour.seekTo(getRandomInt(0,10800));
-    playerFive.seekTo(getRandomInt(0,10800));
 
     player.playVideo();
     playerTwo.playVideo();
     playerThree.playVideo();
     playerFour.playVideo();
-    playerFive.playVideo();
 
     player.unMute();
     playerTwo.unMute();
     playerThree.unMute();
     playerFour.unMute();
-    playerFive.unMute();
     startStopFlag = 'Stop'
     return
   }
@@ -78,7 +67,6 @@ document.querySelector('#wholePage')?.addEventListener('mousedown', function (ev
       playerTwo.pauseVideo();
       playerThree.pauseVideo();
       playerFour.pauseVideo();
-      playerFive.pauseVideo();
   
       startStopFlag = 'Start'
       return
--- scripts/video.js
+++ scripts/video.js
@@ -1,11 +1,9 @@
 "use strict";
 // video vars
-let player, playerTwo, playerThree, playerFour, playerFive;
-let opacity1 = 0,
-  opacity2 = 0,
-  opacity3 = 0;
+let player, playerTwo, playerThree, playerFour;
+let vidElement1, vidElement2, vidElement3, vidElement4;
 let wholeScreen;
-let video1, video2, video3, video4, video5;
+let video1, video2, video3, video4;
 let start, previousTimeStamp;
 let opacityValues = {
   vidOpacity: 0,
@@ -23,32 +21,46 @@ let opacityValues4 = {
   vidOpacity: 0,
   vidReverse: false
 };
-let opacityValues5 = {
-  vidOpacity: 0,
-  vidReverse: false
-};
-// List of random youtube embeded IDs
-let youtubeIDs = ['3rDjPLvOShM','BlVJviK0ci4','9Ej-0VRWmI8','nMAzchVWTis','UuWr5TCbumI']
-
-video1 = document.getElementById('video1');
-video2 = document.getElementById('video2');
-video3 = document.getElementById('video3');
-video4 = document.getElementById('video4');
-video5 = document.getElementById('video5');
-
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
+// for the youtube object  
+let tag, firstScriptTag;
 var playerVars = {
   'autoplay': 0,
   'mute': 1,
   'controls': 0,
   'rel': 0
 }
+
+// list of potential steps for iteration over
+const steps = [0,1,2,5,10,20]
+
+// List of random youtube embeded IDs
+let youtubeIDs = ['lXoH1oQJvHo', '3rDjPLvOShM', 'BlVJviK0ci4', '9Ej-0VRWmI8', 'nMAzchVWTis', 'UuWr5TCbumI']
+
+// rotate potential values
+let rotateValues = ['0', '180']
+
+video1 = document.getElementById('video1');
+video2 = document.getElementById('video2');
+video3 = document.getElementById('video3');
+video4 = document.getElementById('video4');
+
+// to rotate the videos randomly to create interesting overlaps
+vidElement1 = document.getElementById('playerOne');
+vidElement2 = document.getElementById('playerTwo');
+vidElement3 = document.getElementById('playerThree');
+vidElement4 = document.getElementById('playerFour');
+
+vidElement1.style.transform = 'rotate(' + rotateValues[getRandomInt(0, 2)] + 'deg)';
+vidElement2.style.transform = 'rotate(' + rotateValues[getRandomInt(0, 2)] + 'deg)';
+vidElement3.style.transform = 'rotate(' + rotateValues[getRandomInt(0, 2)] + 'deg)';
+vidElement4.style.transform = 'rotate(' + rotateValues[getRandomInt(0, 2)] + 'deg)';
+
+// 2. This code loads the IFrame Player API code asynchronously.
+tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
@@ -69,10 +81,6 @@ function onYouTubeIframeAPIReady() {
     videoId: youtubeIDs[getRandomInt(0, youtubeIDs.length)],
     playerVars: playerVars
   });
-  playerFive = new YT.Player('playerFive', {
-    videoId: youtubeIDs[getRandomInt(0, youtubeIDs.length)],
-    playerVars: playerVars
-  });
 }
 
 function step(timestamp) {
@@ -82,11 +90,10 @@ function step(timestamp) {
 
   if (previousTimeStamp !== timestamp) {
     // iterate over 5 videos with different rates
-    opacityValues = opacityIter(opacityValues, video1, 1);
-    opacityValues2 = opacityIter(opacityValues2, video2, 2);
-    opacityValues3 = opacityIter(opacityValues3, video3, 5);
-    opacityValues4 = opacityIter(opacityValues4, video4, 10);
-    opacityValues5 = opacityIter(opacityValues5, video5, 20);
+    opacityValues = opacityIter(opacityValues, video1, steps[getRandomInt(0,5)]);
+    opacityValues2 = opacityIter(opacityValues2, video2, steps[getRandomInt(0,5)]);
+    opacityValues3 = opacityIter(opacityValues3, video3, steps[getRandomInt(0,5)]);
+    opacityValues4 = opacityIter(opacityValues4, video4, steps[getRandomInt(0,5)]);
   }
   previousTimeStamp = timestamp
   window.requestAnimationFrame(step);
@@ -99,7 +106,7 @@ function opacityIter(opacityValues, element, incrementValue) {
     element.style.opacity = String(opacityValues.vidOpacity / 10000);
     element.style.filter = 'alpha(opacity=' + String(opacityValues.vidOpacity);
     opacityValues.vidOpacity = opacityValues.vidOpacity + incrementValue;
-    if (opacityValues.vidOpacity % 10000 === 0) {
+    if (opacityValues.vidOpacity % 10000 === 0 || opacityValues.vidOpacity > 10000) {
       opacityValues.vidReverse = !opacityValues.vidReverse;
     }
     return {
@@ -113,7 +120,7 @@ function opacityIter(opacityValues, element, incrementValue) {
     element.style.opacity = String(opacityValues.vidOpacity / 10000);
     element.style.filter = 'alpha(opacity=' + String(opacityValues.vidOpacity);
     opacityValues.vidOpacity = opacityValues.vidOpacity - incrementValue;
-    if (opacityValues.vidOpacity === 0) {
+    if (opacityValues.vidOpacity === 0 || opacityValues.vidOpacity < 0) {
       opacityValues.vidReverse = !opacityValues.vidReverse;
     }
     return {
